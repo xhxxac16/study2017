73c48eeba62171fdf14796217ed7ccabb6b5b104
Xia Haixia <xiahaixia@chexiang.com>
2017-02-14 14:01:44 +0800

    第四章 Algorithms and Flow Control 算法和流程控制

--- high-performance-javaScript/common.js
+++ high-performance-javaScript/common.js
@@ -37,3 +37,20 @@ function memoize(fundamental, cache){
     };
     return shell;
 }
+
+
+if (!String.prototype.trim) {
+    // String.prototype.trim = function() {
+    //     return this.replace(/^\s+/, "").replace(/\s+$/, "");
+    // }
+    String.prototype.trim = function() {
+        var str = this.replace(/^\s+/, ""),
+        end = str.length - 1,
+        ws = /\s/;
+        while (ws.test(str.charAt(end))) {
+            end--;
+        }
+        return str.slice(0, end + 1);
+    }
+
+}
